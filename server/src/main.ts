26e09cd425e47f8fdfd68f30178356b46a196c12
Elliot Levin <elliotlevin@hotmail.com>
2020-05-08 23:37:57 +1000

    Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,9 @@ import { TlsRelayServer } from './relay/relay.server';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: ['http://localhost', 'https://debugmypipeline.com'], allowedHeaders: '*' });
+
   const tlsRelay = app.get<TlsRelayServer>('TlsRelayServer');
 
   await Promise.all([app.listen(3000), tlsRelay.listen(3001)]);
