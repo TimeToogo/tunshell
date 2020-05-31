66b2334b0caee5b5a26e081d0e7432b5898eefac
Elliot Levin <elliotlevin@hotmail.com>
2020-05-31 17:29:19 +1000

    Ensure only integeres are returned in attempt direct connect payload

--- server/src/relay/relay.server.ts
+++ server/src/relay/relay.server.ts
@@ -34,7 +34,7 @@ export class TlsRelayServer {
       this.cleanUpIntervalId = setInterval(this.cleanUpOldConnections, this.config.cleanUpInterval);
     });
 
-    return new Promise(resolve => (this.listenResolve = resolve));
+    return new Promise((resolve) => (this.listenResolve = resolve));
   };
 
   public close = async () => {
@@ -49,7 +49,7 @@ export class TlsRelayServer {
       connection.close();
     }
 
-    await new Promise((resolve, reject) => this.server.close(err => (err ? reject(err) : resolve())));
+    await new Promise((resolve, reject) => this.server.close((err) => (err ? reject(err) : resolve())));
     this.server = null;
     this.listenResolve();
     this.listenResolve = null;
@@ -84,7 +84,7 @@ export class TlsRelayServer {
         return;
       }
 
-      const peer = [session.client, session.host].find(i => i.key !== key);
+      const peer = [session.client, session.host].find((i) => i.key !== key);
       const peerConnection =
         this.connections[peer.key] && !this.connections[peer.key].isDead() ? this.connections[peer.key] : null;
 
@@ -129,8 +129,8 @@ export class TlsRelayServer {
       let session: Session & Document;
 
       while ((session = this.sessionsToUpdate.shift())) {
-        session.markModified('host')
-        session.markModified('client')
+        session.markModified('host');
+        session.markModified('client');
         await session.save();
       }
     } finally {
@@ -166,10 +166,10 @@ export class TlsRelayServer {
 
     const maxLatencyMs = Math.max(estimate1.sendLatency, estimate2.sendLatency);
     const bufferMs = 500;
-    const directConnectAttemptTime = Math.round(Date.now() + maxLatencyMs + bufferMs);
+    const directConnectAttemptTime = Date.now() + maxLatencyMs + bufferMs;
 
-    const directConnectTime1 = directConnectAttemptTime + estimate1.timeDiff;
-    const directConnectTime2 = directConnectAttemptTime + estimate2.timeDiff;
+    const directConnectTime1 = Math.round(directConnectAttemptTime + estimate1.timeDiff);
+    const directConnectTime2 = Math.round(directConnectAttemptTime + estimate2.timeDiff);
 
     const [success1, success2] = await Promise.all([
       peer1.attemptDirectConnect(directConnectTime1),
@@ -190,7 +190,7 @@ export class TlsRelayServer {
       return false;
     }
 
-    const participant = [session.client, session.host].find(i => i.key === key);
+    const participant = [session.client, session.host].find((i) => i.key === key);
 
     if (!participant) {
       return false;
