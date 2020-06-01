a425e0663f63690e1d31a5e9983032aca1f4d5d2
Elliot Levin <elliotlevin@hotmail.com>
2020-06-02 01:37:18 +1000

    Tweaking build scripts

--- build/cross-compile.sh
+++ build/cross-compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
+source $HOME/.cargo/env
 
 echo "Parsing targets..."
-TARGETS=$(cat targets.json | jq -r '.[] | [.machine, .arch, .cc, .cc_toolchain, .ldflags, .cflags, .rust_target] | @tsv')
+SCRIPT_DIR=$(dirname "$0")
+TARGETS=$(cat $SCRIPT_DIR/targets.json | jq -r '.[] | [.machine, .arch, .cc, .cc_toolchain, .ldflags, .cflags, .rust_target] | @tsv')
 TARGETS=${TARGETS//$'\t'/,}
-
 
 echo "$TARGETS" | while IFS=',' read -r MACHINE ARCH CC CC_TOOLCHAIN LDFLAGS CFLAGS RUST_TARGET
 do
@@ -14,14 +15,14 @@ do
    export MACHINE
    export ARCH
    export CC
-   export LD=$CC
+   export LD="$CC"
    export LDFLAGS
    export CFLAGS
 
    echo "Installing rust target..."
    rustup target add $RUST_TARGET
 
-   cat > /workspace/dmp-client/.cargo/config <<< EOF
+   cat > /workspace/dmp-client/.cargo/config << EOF
 [target.$RUST_TARGET]
 linker = "$CC"
 EOF
@@ -31,17 +32,19 @@ EOF
    mkdir -p $OPENSSL_BUILD_DIR
    cd /tmp/openssl/
    ./config shared --openssldir=$OPENSSL_BUILD_DIR --prefix=$OPENSSL_BUILD_DIR
-   make install
+   make install_sw
 
    echo "Cross-compiling Libsodium..."
    LIBSODIUM_BUILD_DIR=/tmp/libsodium-build-$CC_TOOLCHAIN/
    mkdir -p $LIBSODIUM_BUILD_DIR
    cd /tmp/libsodium/
+   unset LD
    ./configure --host=$CC_TOOLCHAIN --prefix=$LIBSODIUM_BUILD_DIR
    make install
+   export LD="$CC"
 
-   export OPENSSL_LIB_DIR="$OPENSSL_BUILD_DIR"
-   export OPENSSL_INCLUDE_DIR="/tmp/openssl/include"
+   export OPENSSL_LIB_DIR="$OPENSSL_BUILD_DIR/lib"
+   export OPENSSL_INCLUDE_DIR="$OPENSSL_BUILD_DIR/include"
    export SODIUM_LIB_DIR="$LIBSODIUM_BUILD_DIR/lib"
    export OPENSSL_STATIC=1
    export SODIUM_STATIC=1
