a259ebee070fff37adefa3bd3f5993f20f3b218a
Elliot Levin <elliotlevin@hotmail.com>
2020-07-08 22:12:20 +1000

    Cross-compiling build pipeline configuration

--- build/compile.sh
+++ build/compile.sh
@@ -2,13 +2,21 @@
 
 set -e
 
+TARGETS_FILE=$1
+
+if [[ ! -f "$TARGETS_FILE" ]]; then
+   echo "usage: compile.sh [targets file]"
+   exit 1
+fi
+
 if [[ -f $HOME/.cargo/env ]]; then
    source $HOME/.cargo/env
 fi
 
+
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_DIR=`cd $SCRIPT_DIR;pwd`
-TARGETS=$(cat targets.txt)
+TARGETS=$(cat $TARGETS_FILE)
 
 mkdir -p $SCRIPT_DIR/artifacts
 
@@ -18,6 +26,7 @@ do
 
    echo "Compiling tunshell-client for $TARGET..."
    cd $SCRIPT_DIR/../
-   cross build --release --target $TARGET
+   rustup target add $TARGET
+   cross build  --release --target $TARGET
    cp $SCRIPT_DIR/../target/$TARGET/release/client $SCRIPT_DIR/artifacts/client-$TARGET
 done
