378c79cd2e4515dc6c9f323584d2c3f698e17b21
Elliot Levin <elliotlevin@hotmail.com>
2020-07-08 22:43:32 +1000

    Run all builds in parallel

--- build/compile.sh
+++ build/compile.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-TARGETS_FILE=$1
+TARGET=$1
 
-if [[ ! -f "$TARGETS_FILE" ]]; then
-   echo "usage: compile.sh [targets file]"
+if [[ -z "$TARGET" ]]; then
+   echo "usage: compile.sh [target]"
    exit 1
 fi
 
@@ -16,17 +16,13 @@ fi
 
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_DIR=`cd $SCRIPT_DIR;pwd`
-TARGETS=$(cat $TARGETS_FILE)
 
 mkdir -p $SCRIPT_DIR/artifacts
 
-echo "$TARGETS" | while IFS=',' read -r TARGET
-do
-   echo "Building $TARGET..."
+echo "Building $TARGET..."
 
-   echo "Compiling tunshell-client for $TARGET..."
-   cd $SCRIPT_DIR/../
-   rustup target add $TARGET
-   cross build  --release --target $TARGET
-   cp $SCRIPT_DIR/../target/$TARGET/release/client $SCRIPT_DIR/artifacts/client-$TARGET
-done
+echo "Compiling tunshell-client for $TARGET..."
+cd $SCRIPT_DIR/../
+rustup target add $TARGET
+cross build --release --target $TARGET
+cp $SCRIPT_DIR/../target/$TARGET/release/client $SCRIPT_DIR/artifacts/client-$TARGET
