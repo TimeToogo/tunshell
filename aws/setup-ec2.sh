62ef0d06a14763507480e12b37be4131af3a23a0
Elliot Levin <elliotlevin@hotmail.com>
2020-08-14 20:42:23 +1000

    Fixing cloudformation template and initialisation script

--- aws/setup-ec2.sh
+++ aws/setup-ec2.sh
@@ -1,17 +1,16 @@
 
-#/bin/bash
+#!/bin/bash
 
 set -e
 
 echo "Setting up tunshell server..."
 
-if [[ ! $(which docker) ]];
+if [[ ! -x "$(command -v docker)" ]];
 then
     sudo apt-get update
     sudo apt-get install -y docker.io docker-compose
-    sudo usermod -aG docker $USER
+    sudo usermod -aG docker ubuntu
 fi
-
 
 if [[ ! -f mongo_password ]];
 then
@@ -19,15 +18,26 @@ then
 fi
 
 export MONGO_PASSWORD=$(cat mongo_password)
+export RELAY_DOMAIN=$(cat relay_domain)
 
 curl https://raw.githubusercontent.com/TimeToogo/tunshell/master/aws/docker-compose.yml > docker-compose.yml
 curl https://raw.githubusercontent.com/TimeToogo/tunshell/master/aws/mongo_init.js > mongo_init.js
 curl https://raw.githubusercontent.com/TimeToogo/tunshell/master/aws/nginx.conf > nginx.conf
 
 mkdir -p config/nginx/site-confs/
-mv nginx.conf config/nginx/site-cnfs/default
-o
+mv nginx.conf config/nginx/site-confs/default
 sed -i "s/{{password}}/$MONGO_PASSWORD/g" mongo_init.js
 
 sudo service docker start
-sg docker -c "docker-compose up -d"
+sg docker -c "docker-compose pull"
+# start services gradually as not to run out of memory on small instances
+# and allow extra time for lets encrypt proxy to generate cert 
+sg docker -c "docker-compose up -d reverse_proxy"
+sleep 60
+sg docker -c "docker-compose up -d mongo"
+sleep 30
+sg docker -c "docker-compose up -d relay"
+sleep 30
+sg docker -c "docker-compose up -d watchtower"
+
+echo "done!"
