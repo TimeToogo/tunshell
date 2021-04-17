3d56220d8c1a5f44bf4d01a795e117b3af7f7380
Elliot Levin <elliotlevin@hotmail.com>
2021-04-17 23:16:45 +1000

    init script improvements

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,92 +3,111 @@
 
 set -e
 
-case "$(uname -s):$(uname -m):$(uname -v):$(uname -a)" in
-Linux:x86_64*)     
-    TARGET="x86_64-unknown-linux-musl"
-    ;;
-Linux:aarch64:*:*Android*)
-    TARGET="aarch64-linux-android"
-    ;;
-Linux:arm64*|Linux:aarch64*)
-    TARGET="aarch64-unknown-linux-musl"
-    ;;
-Linux:arm*)     
-    TARGET="armv7-unknown-linux-musleabihf"
-    ;;
-Linux:i686:iSH*)
-    TARGET="ish"
-    ;;
-Linux:i686*)     
-    TARGET="i686-unknown-linux-musl"
-    ;;
-Linux:i586*)     
-    TARGET="i586-unknown-linux-musl"
-    ;;
-Linux:mips*)     
-    TARGET="mips-unknown-linux-musl"
-    ;;
-FreeBSD:x86_64*)
-    TARGET="x86_64-unknown-freebsd"
-    ;;
-FreeBSD:amd64*)
-    TARGET="x86_64-unknown-freebsd"
-    ;;
-FreeBSD:i686*)
-    TARGET="i686-unknown-freebsd"
-    ;;
-Darwin:x86_64*)    
-    TARGET="x86_64-apple-darwin"
-    ;;
-WindowsNT:x86_64*)    
-    TARGET="x86_64-pc-windows-msvc.exe"
-    ;;
-WindowsNT:i686*)    
-    TARGET="i686-pc-windows-msvc.exe"
-    ;;
-*)          
-    echo "Unsupported system ($(uname -a))"
-    exit 1
-    ;;
-esac
+main() {
+    case "$(uname -s):$(uname -m):$(uname -v):$(uname -a)" in
+    Linux:x86_64*)     
+        TARGET="x86_64-unknown-linux-musl"
+        ;;
+    Linux:aarch64:*:*Android*)
+        TARGET="aarch64-linux-android"
+        ;;
+    Linux:arm64*|Linux:aarch64*)
+        TARGET="aarch64-unknown-linux-musl"
+        ;;
+    Linux:arm*)     
+        TARGET="armv7-unknown-linux-musleabihf"
+        ;;
+    Linux:i686:iSH*)
+        TARGET="ish"
+        ;;
+    Linux:i686*)     
+        TARGET="i686-unknown-linux-musl"
+        ;;
+    Linux:i586*)     
+        TARGET="i586-unknown-linux-musl"
+        ;;
+    Linux:mips*)     
+        TARGET="mips-unknown-linux-musl"
+        ;;
+    FreeBSD:x86_64*)
+        TARGET="x86_64-unknown-freebsd"
+        ;;
+    FreeBSD:amd64*)
+        TARGET="x86_64-unknown-freebsd"
+        ;;
+    FreeBSD:i686*)
+        TARGET="i686-unknown-freebsd"
+        ;;
+    Darwin:x86_64*)    
+        TARGET="x86_64-apple-darwin"
+        ;;
+    WindowsNT:x86_64*)    
+        TARGET="x86_64-pc-windows-msvc.exe"
+        ;;
+    WindowsNT:i686*)    
+        TARGET="i686-pc-windows-msvc.exe"
+        ;;
+    *)          
+        echo "Unsupported system ($(uname -a))"
+        exit 1
+        ;;
+    esac
 
-if [ -z "$TMPDIR" ]
-then
-    TMPDIR="/tmp"
-fi
-
-TEMP_PATH="$TMPDIR/tunshell"
-CLIENT_PATH="$TEMP_PATH/client"
-HEADERS_PATH="$TEMP_PATH/headers"
-
-mkdir -p $TEMP_PATH
-
-
-if [ -x "$(command -v curl)" ]
-then
-    INSTALL_CLIENT=true
-
-    # Check if client is already downloaded and up-to-date
-    if [ -x "$(command -v grep)" ] && [ -x "$(command -v cut)" ] && [ -x "$(command -v sed)" ] && [ -f "$HEADERS_PATH" ]
+    if [ ! -z "$XDG_CACHE_HOME" ]
     then
-        CURRENT_ETAG=$(cat $HEADERS_PATH | grep -i etag || true)
-        LATEST_ETAG=$(curl -XHEAD -sSfI https://artifacts.tunshell.com/client-${TARGET} | grep -i etag || true)
+        TEMP_PATH="$XDG_CACHE_HOME"
+    elif [ ! -z "$TMPDIR" ]
+    then
+        TEMP_PATH="$TMPDIR"
+    elif [ -x "$(command -v mktemp)" ]
+    then
+        TEMP_PATH="$(mktemp -d)"
+    else
+        TEMP_PATH="/tmp"
+    fi
 
-        if [ "$CURRENT_ETAG" = "$LATEST_ETAG" ]
+    TEMP_PATH="$TEMP_PATH/tunshell"
+    CLIENT_PATH="$TEMP_PATH/client"
+    HEADERS_PATH="$TEMP_PATH/headers"
+
+    mkdir -p $TEMP_PATH
+
+    if [ ! -O $TEMP_PATH -a -z "$TUNSHELL_INSECURE_EXEC" ];
+    then
+        echo "Temp path $TEMP_PATH is not owned by current user"
+        echo "Run again with TUNSHELL_INSECURE_EXEC=1 to ignore this warning" 
+        exit 1
+    fi
+
+    if [ -x "$(command -v curl)" ]
+    then
+        INSTALL_CLIENT=true
+
+        # Check if client is already downloaded and up-to-date
+        if [ -x "$(command -v grep)" ] && [ -x "$(command -v cut)" ] && [ -x "$(command -v sed)" ] && [ -f "$HEADERS_PATH" ]
         then
-            echo "Client already installed..."
-            INSTALL_CLIENT=false
+            CURRENT_ETAG=$(cat $HEADERS_PATH | grep -i etag || true)
+            LATEST_ETAG=$(curl -XHEAD -sSfI https://artifacts.tunshell.com/client-${TARGET} | grep -i etag || true)
+
+            if [ "$CURRENT_ETAG" = "$LATEST_ETAG" ]
+            then
+                echo "Client already installed..."
+                INSTALL_CLIENT=false
+            fi
         fi
+
+        if [ "$INSTALL_CLIENT" = true ]
+        then
+            echo "Installing client..."
+            curl -sSf https://artifacts.tunshell.com/client-${TARGET} -o $CLIENT_PATH -D $HEADERS_PATH
+        fi
+    else
+        wget https://artifacts.tunshell.com/client-${TARGET} -O $CLIENT_PATH 2> /dev/null
     fi
 
-    if [ "$INSTALL_CLIENT" = true ]
-    then
-        echo "Installing client..."
-        curl -sSf https://artifacts.tunshell.com/client-${TARGET} -o $CLIENT_PATH -D $HEADERS_PATH
-    fi
-else
-    wget https://artifacts.tunshell.com/client-${TARGET} -O $CLIENT_PATH 2> /dev/null
-fi
-chmod +x $CLIENT_PATH
+    chmod +x $CLIENT_PATH
 
-$CLIENT_PATH $1 $2 $3 $4 $5 $6 $7 $8 $9 
+    $CLIENT_PATH "$@"
+}
+
+main "$@" || exit 1
